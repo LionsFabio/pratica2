9010371c3e1a3d7ef4eb862a4be80aa029d75018
Fabio.Storer <fabiostorer19@gmail.com>
2024-06-24 16:36:20 -0300

    feat: adicionado codigo de media

---
+++ index.js
@@ -0,0 +1,8 @@
+numeros = [2, 5, 6, 22, 30, 2, 7, 8];
+soma = 0;
+
+for (let i = 0; i < numeros.length; i++) {
+    soma = soma + numeros[i];
+}
+
+console.log('A média dos números é ', (soma / numeros.length), '!');
