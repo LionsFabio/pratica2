543126962f20ed7325d506a786795b2123912c05
Jean Felipe <JFBimCopinski@gmail.com>
2024-06-24 18:22:16 -0300

    added: moda e switch

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
-numeros = [2, 5, 6, 22, 30, 2, 7, 8, 3, 42];
-soma = 0;
+const numeros = [2, 5, 6, 22, 30, 2, 7, 8, 3, 42];
+const moda = [];
+let input;
+let moda1;
 
-for (let i = 0; i < numeros.length; i++) {
-    soma = soma + numeros[i];
-}
-
-console.log('A média dos números é ', (soma / numeros.length), '!');
+console.log("Qual cálculo deseja realizar?");
+console.log(
+  'Tecle "1" para a média\nTecle "2" para a mediana\nTecle "3" para a moda'
+);
+process.stdin.on("data", function (data) {
+  input = data.toString().trim();
+  switch (input) {
+    case "1":
+      break;
+    case "2":
+      break;
+    case "3":
+      for (let i = 0; i < numeros.length; i++) {
+        for (let j = i + 1; j < numeros.length; j++) {
+          if (numeros[i] > numeros[j]) {
+            let aux = numeros[i];
+            numeros[i] = numeros[j];
+            numeros[j] = aux;
+          }
+          if (numeros[i] === numeros[j]) {
+            moda.push(numeros[i]);
+            console.log("A moda é: " + moda);
+          }
+        }
+      }
+      console.log(numeros);
+      break;
+  }
+});
